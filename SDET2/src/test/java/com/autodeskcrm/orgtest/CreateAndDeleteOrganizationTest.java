d74a5c82e7b428697010e7fff9eeff4b8f083088
deepak-biswal <dkb.040790@gmail.com>
2020-08-03 18:00:08 +0530

    First Commit By Deepak

---
+++ SDET2/src/test/java/com/autodeskcrm/orgtest/CreateAndDeleteOrganizationTest.java
@@ -0,0 +1,35 @@
+package com.autodeskcrm.orgtest;
+
+import org.testng.annotations.Test;
+
+import com.autodeskcrm.gerericutils.ExcelLib;
+import com.autodeskcrm.gerericutils.FileLib;
+import com.autodeskcrm.gerericutils.WebDriverUtils;
+
+/**This Class will Create and delete the Organization
+ * @author Deepak
+ *
+ */
+public class CreateAndDeleteOrganizationTest {
+
+	@Test
+	public void createAndDeletOrg() {
+		WebDriverUtils wLib = new WebDriverUtils();
+		FileLib fLib = new FileLib();
+		ExcelLib excelLib = new ExcelLib();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
